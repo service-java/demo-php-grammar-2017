5732960a81bb5588873c79d550d51bca68a8988f
luo0412 <1095847440@qq.com>
2017-03-19 20:08:45 +0800

    调整文件结构

---
+++ ajax/xml-json/xml_basic.php
@@ -0,0 +1,9 @@
+<?php
+# @Author: 骆金参
+# @Date:   2017-03-19T20:05:59+08:00
+# @Email:  1095947440@qq.com
+# @Filename: xml_basic.php
+# @Last modified by:   骆金参
+# @Last modified time: 2017-03-19T20:05:59+08:00
+
+
